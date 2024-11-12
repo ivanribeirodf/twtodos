07cb9278574657191719a521f536869e711dd214
IvanPix <ivan@pix.com.br>
2024-11-12 20:34:05 -0300

    Estrutura inicio do projeto CadEvent

---
+++ src/main/java/com/ivanribeiro/twtodos/models/Todo.java
@@ -0,0 +1,100 @@
+package com.ivanribeiro.twtodos.models;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Todo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String title;
+	
+	@Column(nullable = false)
+	private LocalDateTime createAt;
+	
+	@Column(nullable = false)
+	private LocalDate deadLine ;
+	
+	@Column(nullable = true)
+	private LocalDate fineshedAt;
+	
+	public Todo() {
+		this.createAt = LocalDateTime.now();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public LocalDateTime getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(LocalDateTime createAt) {
+		this.createAt = createAt;
+	}
+
+	public LocalDate getDeadLine() {
+		return deadLine;
+	}
+
+	public void setDeadLine(LocalDate deadLine) {
+		this.deadLine = deadLine;
+	}
+
+	public LocalDate getFineshedAt() {
+		return fineshedAt;
+	}
+
+	public void setFineshedAt(LocalDate fineshedAt) {
+		this.fineshedAt = fineshedAt;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Todo other = (Todo) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Todo [id=" + id + ", title=" + title + "]";
+	}
+	
+	
+
+}
---
+++ src/main/java/com/ivanribeiro/twtodos/repositories/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.ivanribeiro.twtodos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ivanribeiro.twtodos.models.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+}
